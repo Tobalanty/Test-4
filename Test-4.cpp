537947488980ccbcb31b1b553e1ef3fdc529546b
Tobalanty <gg1gg141@gmail.com>
2020-11-26 21:49:13 +0300

    Some changes
    1. Change if-else to switch
    2. Add comments

--- Test-4.cpp
+++ Test-4.cpp
@@ -2,14 +2,14 @@
 #include <string>
 
 enum class MONSTER_TYPE{
-    OGRE,
-    GOBLIN,
-    SKELETON,
-    ORC,
-    TROLL,
+    OGRE,       //0
+    GOBLIN,     //1
+    SKELETON,   //2
+    ORC,        //3
+    TROLL,      //4
 };
 
-struct Stats {
+struct Stats {                                                                           //Структура содержащая тип монстра, имя и количество хп
 
     MONSTER_TYPE monster_type;
     std::string name;
@@ -18,40 +18,24 @@ struct Stats {
 
 };
 
-std::string typeChoose(MONSTER_TYPE b) {
-
+std::string typeChoose(MONSTER_TYPE b) {                                                //Выбираю и записываю в строковую переменную тип монстра в зависимости 
+                                                                                        //от выбраного типа в переменной класса MONSTER_TYPE
     std::string type;
 
-    if (static_cast<int>(b) == 0) {
+    switch (static_cast<int>(b)) {
 
-        type = "ogre";
+    case 0: type = "ogre";
+    case 1: type = "goblin";
+    case 2: type = "sceleton";
+    case 3: type = "orc";
+    case 4: type = "troll";
 
-    }else 
-        if (static_cast<int>(b) == 1) {
-
-        type = "goblin";
-
-    }else 
-         if (static_cast<int>(b) == 2) {
-
-        type = "sceleton";
-
-    }else 
-         if (static_cast<int>(b) == 3) {
-
-        type = "orc";
-
-    }else 
-         if (static_cast<int>(b) == 4) {
-
-        type = "troll";
-
-         }
-
+    }
     return type;
 }
+ 
 
-Stats input(Stats a) {
+Stats input(Stats a) {                                                                  //Ввод имени и количества хп
 
     a.monster_type_string = typeChoose(a.monster_type);
     std::cout << "Input " << a.monster_type_string << "'s name: ";
@@ -62,18 +46,18 @@ Stats input(Stats a) {
     return a;
 }
 
-void output(Stats stats) {
+void output(Stats stats) {                                                              //Вывод резальтата             
 
-    std::cout << "This " << stats.monster_type_string << " is named " << stats.name << " and has "
-        << stats.health << " hp." << "\n";
+    std::cout << "This " << stats.monster_type_string << " is named " 
+        << stats.name << " and has "<< stats.health << " hp." << "\n";
 
 }
 
 int main()
 {
     
-    Stats first{ MONSTER_TYPE::GOBLIN }, second{ MONSTER_TYPE::ORC };
-
+    Stats first{ MONSTER_TYPE::GOBLIN }, second{ MONSTER_TYPE::ORC };                   //Инициализирую переменную класса MONSTER_TYPE сразу с объявление структурной перменной
+                                                                                        //(остальные переменные структуры пока что пустые)
     first = input(first);
     std::cout << "---------------------------------------------------\n"
               << "---------------------------------------------------\n";
